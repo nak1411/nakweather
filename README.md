1c45489031d0d488b79a429edb766bc90c9639c7
Justin <nak1411@gmail.com>
2018-10-18 22:22:42 -0700

    Documentation update

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# NAKWEATHER
-## Visual weather app using OpenWeather, Google Geolocation
-## and Google Timezone APIs.
+# NakWeather
+
+A real-time visual weather app using OpenWeather and Google Map APIs. 
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Support](#support)
+- [Contributing](#contributing)
+
+## Installation
+
+
+## Usage
+
+
+## Support
+
+Please [open an issue](https://github.com/nak1411/nakweather/issues/new) for support.
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits, and [open a pull request](https://github.com/nakdev/nakweather/compare/).
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+/**
+ * © 2018 Justin Schlump
+ * NakDev Webpack Config File
+ */
+
+//Update imports as needed per project 
 const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
@@ -7,12 +13,14 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProd = process.env.NODE_ENV === 'production';
-const toolProd = '';
-const toolDev = 'eval';
+//Update plugins as needed per project
 const pluginsProd = [new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), new BundleAnalyzerPlugin(), new MiniCssExtractPlugin({filename: 'main.css'}), new MinifyPlugin(), new OptimizeCSSAssetsPlugin(), new HtmlWebpackPlugin({template: 'src/index.html'})];
 const pluginsDev = [new HtmlWebpackPlugin({template: 'src/index.html'}), new MiniCssExtractPlugin({filename: 'main.css'}), new HardSourceWebpackPlugin()];
 
+//Checking mode to apply correct settings
+const isProd = process.env.NODE_ENV === 'production';
+const toolProd = '';
+const toolDev = 'eval';
 const plugins = isProd ? pluginsProd : pluginsDev;
 const devtool = isProd ? toolProd : toolDev;
 
